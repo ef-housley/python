8c05b50ae595f373a7df00ea413e269debf9c60e
ef-housley <103453370+ef-housley@users.noreply.github.com>
2022-04-13 12:02:38 +0300

    Add files via upload

---
+++ ch_2_name_cases.py
@@ -0,0 +1,25 @@
+# page nr 29
+# 2-3
+name = "Eric"
+print("Hello " + name + " would you like to learn some Python today ?")
+
+# 2-4
+name = "jane"
+print(name.upper())
+print(name.lower())
+print(name.title())
+
+# 2-5
+print('Albert Einstein once said, "A person who never made a mistake never tried anything new."')
+
+# 2-6
+famous_person = "Albert Einstein"
+message = "A person who never made a mistake never tried anything new."
+print(famous_person + " - " + message)
+
+# 2-7
+name = "\n effie housley \t"
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip())
