af7465ee145b0fea35e8820739cb5e297fe6f5e1
ef-housley <103453370+ef-housley@users.noreply.github.com>
2022-04-13 12:03:14 +0300

    Add files via upload

---
+++ ch_2_numbers.py
@@ -0,0 +1,14 @@
+# 2-8
+print(4+4)
+print(16/2)
+print(4*2)
+print(24-16)
+
+# 2-9
+
+favourite_number = 8
+message = "our favourite number is: " + str(favourite_number)
+print(message)
+
+# 2-10 adding comments
+
